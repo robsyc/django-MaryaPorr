c82a17f4481203049bca1104c68875096f62f3eb
Robbe C <87700333+robsyc@users.noreply.github.com>
2024-10-19 10:35:29 +0200

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -42,7 +42,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 BOOTSTRAP5 = {
-    "theme_url": "/static/css/bootstrap.css",
+    "css_url": STATIC_ROOT + "/css/bootstrap.css"
+    # "theme_url": "/static/css/bootstrap.css",
 }
 
 # Application definition
