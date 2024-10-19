9cd44a9c9d231654b192c15e9d4b8d74adbd3bf7
robsyc <robbe.claeys@gmail.com>
2024-10-19 14:06:41 +0200

    Add MEDIA_URL and MEDIA_ROOT to settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,8 +141,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "static/"
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 BOOTSTRAP5 = {
     "css_url": "/static/css/bootstrap.css",
     # "css_url": os.path.join(STATIC_ROOT, "/static/css/bootstrap.css"),
