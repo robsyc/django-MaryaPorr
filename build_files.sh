ed16766b0cc39b1917e84ceef3b1c6f5b2c86ac9
robsyc <robbe.claeys@gmail.com>
2024-10-19 12:43:07 +0200

    trying again for Vercel

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,8 @@ echo "BUILD START"
 python3.9 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-python manage.py collectstatic --noinput 
+python manage.py collectstatic --noinput
+pwd
+ls
 
 echo "BUILD END"
