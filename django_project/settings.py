a29a1a542ca3048192415c22325d1db3bd248138
robsyc <robbe.claeys@gmail.com>
2024-10-19 12:22:44 +0200

    checking if this works on Vercel

--- django_project/settings.py
+++ django_project/settings.py
@@ -144,6 +144,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 BOOTSTRAP5 = {
-    "css_url": STATIC_URL + "css/bootstrap.css"
+    "css_url": os.path.join(BASE_DIR, "/static/css/bootstrap.css"),
     # "theme_url": "/static/css/bootstrap.css",
 }
